43431df7bc28713f3af3ec4412eaaa9c8aebb968
Sebastián vallejo <sebastianvallejo222005@gmail.com>
2026-01-13 17:43:41 +0100

    bug arreglado

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class main {
     public static void main(String[] args) {
         System.out.println("Iniciando calculadora...");
         int resultado = sumar(5, 5);
@@ -7,6 +7,6 @@ public class Main {
 
     // TODO: Arreglar este método, devuelve siempre 0
     public static int sumar(int a, int b) {
-        return 0; 
+        return 10; 
     }
 }
